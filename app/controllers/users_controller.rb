94232c71ab8e15b6cf0b2bcaaa5caddac3600970
Lee Mckenna <kenna12345@hotmail.co.uk>
2024-03-22 17:12:13 +0100

    Edit function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show]
   def show
     #--
+    @current_user = current_user
     @bookings = current_user.bookings
     @items = current_user.items
   end
